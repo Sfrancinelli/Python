2f18b38a67d0c1ddde66fc78b5569bb29ca0fed3
Sebastián Francinelli <112777037+Sfrancinelli@users.noreply.github.com>
2022-11-03 12:17:53 -0300

    Add files via upload

--- Udemy/day4/treasure_map2.py
+++ Udemy/day4/treasure_map2.py
@@ -11,8 +11,10 @@ position = input("Where do you want to put the treasure? ")
 horizontal = int(position[0])
 vertical = int(position [1])
 
-selected_row=map[vertical - 1]
-selected_row[horizontal - 1] = "X"
+# selected_row = map[vertical - 1]
+# selected_row[horizontal - 1] = "X"
+
+map[vertical - 1][horizontal - 1] = "X"
 #Write your code above this row 👆
 
 # 🚨 Don't change the code below 👇
