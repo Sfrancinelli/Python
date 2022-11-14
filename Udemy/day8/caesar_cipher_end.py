8ddd486a72d9fc8eb3398bbb50a792f0956201f2
Sebastián Francinelli <112777037+Sfrancinelli@users.noreply.github.com>
2022-11-14 09:53:48 -0300

    caesar_cipher_end (cut unnecesary lines)

--- Udemy/day8/caesar_cipher_end.py
+++ Udemy/day8/caesar_cipher_end.py
@@ -1,34 +1,18 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 def caesar(text,shift,direction):
-    cipher_text = ""
-    decoded_text = ""
-    for char in text:
-        for i in alphabet:
-            if char == i:
-                index = alphabet.index(char)
-                if direction == "encode":
-                    index += shift
-                    if index >= len(alphabet):
-                        rest = index - len(alphabet)
-                        index = rest
-                    coded = alphabet[index]
-                    cipher_text += coded
-                elif direction == "decode":
-                    index -= shift
-                    if index < 0:
-                        rest = len(alphabet) + index 
-                        index = rest
-                    decoded = alphabet[index]
-                    decoded_text += decoded
-    if direction == "encode":
-        print(f"The encoded text is {cipher_text} and the shift number is {shift}")
-    elif direction == "decode":
-        print(f"The decoded text is {decoded_text} and the shift number is {shift}")
+    end_text = ""
+    if direction == "decode":
+        shift *= -1
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        end_text += alphabet[new_position]
+    print(f"The {direction}d text is {end_text}")
 
 if direction == "encode" or direction == "decode":
     caesar(text,shift,direction)
