8a6c030034f5dcc49688819eb516a935d9364e27
Sebastián Francinelli <112777037+Sfrancinelli@users.noreply.github.com>
2022-11-14 12:33:51 -0300

    Caesar cipher completed (art and reset added)

--- Udemy/day8/caesar_cipher_end.py
+++ Udemy/day8/caesar_cipher_end.py
@@ -1,18 +1,35 @@
+from caesar_art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
 
 def caesar(text,shift,direction):
     end_text = ""
+    if shift > 26:
+        shift = shift % 26
     if direction == "decode":
         shift *= -1
-    for letter in text:
-        position = alphabet.index(letter)
-        new_position = position + shift
-        end_text += alphabet[new_position]
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
     print(f"The {direction}d text is {end_text}")
 
-if direction == "encode" or direction == "decode":
-    caesar(text,shift,direction)
+reset = 'yes'
+
+while reset == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if direction == "encode" or direction == "decode":
+        caesar(text,shift,direction)
+
+    reset = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+
+print("Goodbye!")
+
