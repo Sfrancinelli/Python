26b6e68954248697449cc3ce7cfa560db0c68483
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2022-12-21 16:19:21 -0300

    removed documents from Codo a Codo to stablish some order

--- Udemy/class_abstracta_pt2.py
+++
@@ -1,9 +0,0 @@
-from class_abstracta import Animal
-
-class Gato(Animal):
-    def mover(self):
-        print('El gato se mueve.')
-
-    def emitir_sonido(self):
-        super().emitir_sonido()
-        print('Miau!')
--- Udemy/class_abstracta_pt3.py
+++
@@ -1,9 +0,0 @@
-from class_abstracta import Animal
-
-class Perro(Animal):
-    def mover(self):
-        print('El perro se mueve.')
-
-    def emitir_sonido(self):
-        super().emitir_sonido()
-        print('Guau!')
