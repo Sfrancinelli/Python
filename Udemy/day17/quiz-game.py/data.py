30fb7b02d15898c86d1e9e3a35def1f6df0161d8
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2022-12-26 12:15:03 -0300

    Added open trivia database link, for more question

--- Udemy/day17/quiz-game.py/data.py
+++ Udemy/day17/quiz-game.py/data.py
@@ -1,3 +1,4 @@
+# For more questions, visit https://opentdb.com/api_config.php or visit https://opentdb.com/
 question_data = [
 {"text": "A slug's blood is green.", "answer": "True"},
 {"text": "The loudest animal is the African Elephant.", "answer": "False"},
