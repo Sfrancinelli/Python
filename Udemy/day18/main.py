1e2cca0df02d2942d89d2b3bba1d4689ed8c0436
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2022-12-27 15:21:53 -0300

    Done the same as before but with a function

--- Udemy/day18/main.py
+++ Udemy/day18/main.py
@@ -7,55 +7,67 @@ colormode(255)
 tim.shape("turtle")
 tim.color("coral")
 
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.fd(100)
+        tim.left(angle)
 
-tim.color(randint(0, 255),
-          randint(0, 255),
-          randint(0, 255))
-for i in range(4):
-    tim.fd(100)
-    tim.left(90)
+for shape_side_n in range(3,11): # From a triangle (3 sides) to a decagon (10 sides)!
+    tim.color(randint(0, 255),
+              randint(0, 255),
+              randint(0, 255))
+    draw_shape(shape_side_n)
 
-tim.color(randint(0, 255),
-          randint(0, 255),
-          randint(0, 255))
-for i in range(5):
-    tim.fd(100)
-    tim.left(72)
 
-tim.color(randint(0, 255),
-          randint(0, 255),
-          randint(0, 255))
-for i in range(6):
-    tim.fd(100)
-    tim.left(60)
+# tim.color(randint(0, 255),
+#           randint(0, 255),
+#           randint(0, 255))
+# for i in range(4):
+#     tim.fd(100)
+#     tim.left(90)
 
-tim.color(randint(0, 255),
-          randint(0, 255),
-          randint(0, 255))
-for i in range(7):
-    tim.fd(100)
-    tim.left(360/7)
+# tim.color(randint(0, 255),
+#           randint(0, 255),
+#           randint(0, 255))
+# for i in range(5):
+#     tim.fd(100)
+#     tim.left(72)
 
-tim.color(randint(0, 255),
-          randint(0, 255),
-          randint(0, 255))
-for i in range(8):
-    tim.fd(100)
-    tim.left(360/8)
+# tim.color(randint(0, 255),
+#           randint(0, 255),
+#           randint(0, 255))
+# for i in range(6):
+#     tim.fd(100)
+#     tim.left(60)
 
-tim.color(randint(0, 255),
-          randint(0, 255),
-          randint(0, 255))
-for i in range(9):
-    tim.fd(100)
-    tim.left(360/9)
+# tim.color(randint(0, 255),
+#           randint(0, 255),
+#           randint(0, 255))
+# for i in range(7):
+#     tim.fd(100)
+#     tim.left(360/7)
 
-tim.color(randint(0, 255),
-          randint(0, 255),
-          randint(0, 255))
-for i in range(10):
-    tim.fd(100)
-    tim.left(36)
+# tim.color(randint(0, 255),
+#           randint(0, 255),
+#           randint(0, 255))
+# for i in range(8):
+#     tim.fd(100)
+#     tim.left(360/8)
+
+# tim.color(randint(0, 255),
+#           randint(0, 255),
+#           randint(0, 255))
+# for i in range(9):
+#     tim.fd(100)
+#     tim.left(360/9)
+
+# tim.color(randint(0, 255),
+#           randint(0, 255),
+#           randint(0, 255))
+# for i in range(10):
+#     tim.fd(100)
+#     tim.left(36)
 
 
 
