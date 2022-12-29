74e27e095248cc187f3ad620459c285f1aabc1c7
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2022-12-29 12:00:35 -0300

    Got Snake to move continuosly together!

--- Udemy/day20/snake_game/main.py
+++ Udemy/day20/snake_game/main.py
@@ -6,27 +6,33 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-snakes = []
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
 
 x = 0
 
-for _ in range(3):
-    snake = Turtle()
-    snake.shape("square")
-    snake.color("white")
-    snake.penup()
-    snake.goto(x, 0)
-    x -= 20
-    snakes.append(snake)
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
 screen.update()
 
 game_is_on = True
 while game_is_on:        
     screen.update()
-    time.sleep(.1)
-    for snake in snakes:
-        snake.fd(20)
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].fd(20)
+    segments[0].left(90)
+        
         
 
 screen.exitonclick()
