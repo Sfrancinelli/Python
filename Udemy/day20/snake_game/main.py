e273b83ade43bf6a089261842cbe611422760217
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2022-12-29 12:04:03 -0300

    Tail following the head on snake game

--- Udemy/day20/snake_game/main.py
+++ Udemy/day20/snake_game/main.py
@@ -31,7 +31,6 @@ while game_is_on:
         new_y = segments[seg_num - 1].ycor()
         segments[seg_num].goto(new_x, new_y)
     segments[0].fd(20)
-    segments[0].left(90)
         
         
 
