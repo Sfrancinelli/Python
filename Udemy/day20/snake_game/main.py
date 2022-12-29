9781be85a2a623107b86aef9d5bb7b2084ccc492
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2022-12-29 17:26:05 -0300

    Snake Game completed!!!!!!!

--- Udemy/day20/snake_game/main.py
+++ Udemy/day20/snake_game/main.py
@@ -40,10 +40,10 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect collision with tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 8:
+    for segment in snake.segments[1:]: # Slicing
+        # if segment == snake.head:
+        #     pass
+        if snake.head.distance(segment) < 8:
             game_is_on = False
             scoreboard.game_over()
       
