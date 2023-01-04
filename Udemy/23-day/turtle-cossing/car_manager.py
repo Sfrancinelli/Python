e8fb1f81d7ecb5fa53f89e12e400800c1d84e5da
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-04 11:16:49 -0300

    Game working, got to detect colission with cars!

--- Udemy/23-day/turtle-cossing/car_manager.py
+++ Udemy/23-day/turtle-cossing/car_manager.py
@@ -14,5 +14,23 @@ class CarManager(Turtle):
         self.goto(300, randint(-250, 250))
         self.starting_speed = STARTING_MOVE_DISTANCE
         self.speeding = MOVE_INCREMENT
+        self.level = 0
+        self.setheading(180)
+        self.shape("square")
+        self.shapesize(stretch_len=2)
 
     
+    def move(self):
+        self.fd(self.starting_speed)
+
+    def level_up(self):
+        self.level += 1
+        self.increase_speed()
+
+    def increase_speed(self):
+        self.starting_speed += self.speeding
+        self.move()
+
+    def reset_position(self):
+        self.goto(300, randint(-250, 250))
+        self.move()
--- Udemy/23-day/turtle-cossing/main.py
+++ Udemy/23-day/turtle-cossing/main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+from random import randint, choice
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -16,15 +17,25 @@ screen.listen()
 screen.onkeypress(key="Up", fun=player.up)
 screen.onkeypress(key="Down", fun=player.down)
 
+cars = []
+
+for _ in range(randint(0,20)):
+    car = CarManager()
+    cars.append(car)
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    choice(cars).move()
 
     if player.ycor() > 275:
         scoreboard.level_up()
         player.next_level()
+        for car in cars:
+            car.level_up()
+            car.reset_position()
 
 
 screen.exitonclick()
