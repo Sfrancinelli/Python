e2bcb7a955243ccf5ed894c1c568eacd2653a9db
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-10 15:38:42 -0300

    learning about keyword arguments (**kw)

--- Udemy/27-day/aguments_func.py
+++ Udemy/27-day/aguments_func.py
@@ -46,6 +46,8 @@ class Car:
     def __init__(self, **kw):
         self.make = kw.get("make")
         self.model = kw.get("model")
+        self.colour = kw.get("colour")
+        self.seats = kw.get("seats")
 
 my_car = Car(make="Nissan")
 print(my_car.model)
