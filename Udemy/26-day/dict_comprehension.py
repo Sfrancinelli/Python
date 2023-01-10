4647f709c535957bf6e5ca21fd0754775ce1d10b
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-10 11:29:15 -0300

    Excercise 2 completed!

--- Udemy/26-day/dict_comprehension.py
+++ Udemy/26-day/dict_comprehension.py
@@ -35,4 +35,19 @@ result = {word : len(word) for word in list_of_words}
 
 print(result)
 
-# Exercise 2: 
+# Exercise 2: Use dictionary comprehension to create a dict called weather_f
+# that takes each temperature in degrees Celcius and converts it into Farenheight
+
+weather_c = {
+    "Monday" : 12,
+    "Tuesday" : 14,
+    "Wednesday" : 15,
+    "Thursday" : 14,
+    "Friday" : 21,
+    "Saturday" : 22,
+    "Sunday" : 24,
+}
+
+weather_f = {day : (temp * 9/5) + 32 for (day, temp) in weather_c.items()}
+
+print(weather_f)
