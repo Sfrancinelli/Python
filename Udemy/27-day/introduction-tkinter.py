33de353ef6bdc9d0fc04e976acee7197ca6b951a
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-10 16:13:10 -0300

    Learning some Tkinter widgets (label, entry, buttons)

--- Udemy/27-day/introduction-tkinter.py
+++ Udemy/27-day/introduction-tkinter.py
@@ -1,12 +1,33 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 
 window.title("First GUI")
 window.minsize(width=500, height=300)
 
 # Label creation
-label = tkinter.Label(text="I am a Label", font=("Courier", 24, "italic"))
-label.pack(expand=True)
+label = Label(text="I am a Label", font=("Courier", 24, "italic"))
+label.pack()
+
+# Update properties of a created object
+label["text"] = "New text"
+label.config(text="New Text")
+
+# Button
+def button_clicked():
+    print("I got clicked")
+    # Get the label to display what's on the Entry
+    label.config(text=input.get())
+
+button = Button(text="Click ME", command=button_clicked)
+button.pack()
+
+# Entry 
+
+input = Entry(width=10)
+input.pack()
+# Get the Entry input
+print(input.get())
+
 
 window.mainloop()
