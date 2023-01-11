10c111f5e9c82a101679e7a9258794dc5f3e4ab2
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-11 15:01:20 -0300

    Continuar programa pomodoro

--- Udemy/28-day/main.py
+++ Udemy/28-day/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,16 +16,26 @@ def reset():
     pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start():
-    pass
+    countdown(1500)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(t):
 
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        timer = str(timer)
+        timer_label.config(text=timer)
+        time.sleep(1)
+        timer = int(timer)
+        t -= 1
+        
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
-timer.grid(column=1, row=0)
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column=1, row=0)
 
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -33,10 +44,10 @@ canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Calculate", command=start, justify="right")
+start_button = Button(text="Start", command=start, justify="right")
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Calculate", command=reset, justify="left")
+reset_button = Button(text="Reset", command=reset, justify="left")
 reset_button.grid(column=2, row=2)
 
 check = Label(text=CHECK_MARK*3, fg=GREEN, bg=YELLOW)
