71b51d6ad25639073ae0618be5d9d1212048354d
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-11 12:38:52 -0300

    UI for Pomodoro App ready, working on functions

--- Udemy/28-day/main.py
+++ Udemy/28-day/main.py
@@ -8,11 +8,14 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECK_MARK = "✔"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset():
+    pass
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start():
+    pass
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -20,10 +23,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer.grid(column=1, row=0)
+
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="Udemy/28-day/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Calculate", command=start, justify="right")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Calculate", command=reset, justify="left")
+reset_button.grid(column=2, row=2)
+
+check = Label(text=CHECK_MARK*3, fg=GREEN, bg=YELLOW)
+check.grid(column=1, row=3)
 
 window.mainloop()
