4fa4220a6b43e80d0c19e903df7e884b07946647
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-11 12:29:11 -0300

    Background and image of the Pomodoro App ready

--- Udemy/28-day/main.py
+++ Udemy/28-day/main.py
@@ -18,12 +18,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="Udemy/28-day/tomato.png")
-canvas.create_image(102, 112, image=tomato_img)
-canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 window.mainloop()
