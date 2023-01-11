c8adaf0804ed645285f51097b885ad4aa6676393
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-11 10:40:14 -0300

    Starting the pomodoro app with Tkinter

---
+++ Udemy/28-day/main.py
@@ -0,0 +1,29 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50)
+
+canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="Udemy/28-day/tomato.png")
+canvas.create_image(102, 112, image=tomato_img)
+canvas.create_text(102, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+window.mainloop()
