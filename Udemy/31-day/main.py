5723c3584ecee373f3fe0f9b2031853e360b0034
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-13 17:39:53 -0300

    Seguir con el proyecto de Flash Card!

--- Udemy/31-day/main.py
+++ Udemy/31-day/main.py
@@ -1,9 +1,41 @@
 from tkinter import *
+import pandas
+import math
 
 BACKGROUND_COLOR = "#B1DDC6"
 language = "French"
-
 word = "something for now"
+
+# ---------------------------- CSV READINFG ----------------------------
+
+data_frame = pandas.read_csv("Udemy/31-day/data/french_words.csv")
+
+data = data_frame.to_dict(orient="records")
+
+print(data)
+
+# ---------------------------- TIMER --------------------------------
+
+def countdown(count):
+
+    global check_mark_multiplier
+
+    count_min = math.floor(count / 60)
+
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, countdown, count -1)
+    else:
+        start()
+        if reps % 2 == 0:
+            check.config(text=f"{CHECK_MARK*check_mark_multiplier}")
+            check_mark_multiplier += 1
+       
 
 # ---------------------------- UI INTERFACE ----------------------------
 
