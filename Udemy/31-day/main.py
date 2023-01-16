43fe79c37335ae0b774fdd6992be84338c55159a
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-16 11:29:53 -0300

    Flip card functon ready, got to save progress now!

--- Udemy/31-day/main.py
+++ Udemy/31-day/main.py
@@ -2,10 +2,10 @@ from tkinter import *
 import pandas
 import math
 import random
-from functools import partial
 
 BACKGROUND_COLOR = "#B1DDC6"
-language = "French"
+language_title = "French"
+current_card = {}
 
 # ---------------------------- CSV READINFG ----------------------------
 
@@ -18,49 +18,44 @@ print(data[0]['French']) # Format to acces french words
 print(data[0]['English'])
 
 print(len(data)) # 101
-
-# ---------------------------- TIMER --------------------------------
-
-def countdown(count):
-    """Counts the number of seconds especified by the parameter (count) and changes the text of the canvas"""
-    count_min = math.floor(count / 60)
-
-    count_sec = count % 60
-    if count_sec < 10:
-        count_sec = f"0{count_sec}"
-
-    if count > 0:
-        global timer
-        timer = window.after(1000, countdown, count -1)
-        if count % 3 == 0:
-                change_word('french')
-                print("changing")
-
+ 
 # ---------------------------- Change Word ----------------------------
 
-def change_word(languague):
-    """Changes the word of the canvas for another random one. The input language has to be in lowercase"""
-    
-    random_num = random.randint(0,100)
+def change_word():
+    """Changes the word of the canvas for another random one."""
 
-    if languague == "english":
-        english_word = data[random_num]['English']
-        print(english_word)
-        canvas.itemconfig(word_text, text=english_word)
-    elif languague == "french":
-        french_word = data[random_num]['French']
-        print(french_word)
-        canvas.itemconfig(word_text, text=french_word)
+    global language_title, current_card, flip_timer
 
+    window.after_cancel(flip_timer)
+    current_card = random.choice(data)
+    # Changing title to 'French' and a random new french word
+    language_title = "French"
+    canvas.itemconfig(title_text, text = language_title, fill="black")
+    french_word = current_card["French"]
+    canvas.itemconfig(word_text, text=french_word, fill="black")
+    canvas.itemconfig(card_background, image=canvas_img_front)
+
+    flip_timer = window.after(3000, func=flip_card)
+
+# ---------------------------- FLIP CARD ----------------------------
+
+def flip_card():
+    global language_title, current_card
+
+    language_title = "English"
+    # canvas.create_image(400, 263, image=canvas_img_back)
+    canvas.itemconfig(title_text, text =language_title, fill="white")
+    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=canvas_img_back)
+            
 # ---------------------------- UI INTERFACE ----------------------------
-
-# function_french = change_word("french")
-# function_english = change_word("english")
 
 # Root
 window = Tk()
 window.title("Flash Card Proyect")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+
+flip_timer = window.after(3000, func=flip_card)
 
 # Canvas
 canvas_img_front = PhotoImage(file="Udemy/31-day/images/card_front.png")
@@ -68,23 +63,23 @@ canvas_img_front = PhotoImage(file="Udemy/31-day/images/card_front.png")
 canvas_img_back = PhotoImage(file="Udemy/31-day/images/card_back.png")
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
-canvas.create_image(400, 263, image=canvas_img_front)
+card_background = canvas.create_image(400, 263, image=canvas_img_front)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Canvas text
-title_text = canvas.create_text(400, 150, text=language, fill="black", font=("Ariel", 40, "italic"))
+title_text = canvas.create_text(400, 150, text=language_title, fill="black", font=("Ariel", 40, "italic"))
 
 word_text = canvas.create_text(400, 263, text="", fill="black", font=("Ariel", 60, "bold"))
 
 # Buttons
 cross_image = PhotoImage(file="Udemy/31-day/images/wrong.png")
-cross_button = Button(image=cross_image, highlightthickness=0, command=partial(change_word, "english"))
+cross_button = Button(image=cross_image, highlightthickness=0, command=change_word)
 cross_button.grid(row=1, column=0)
 
 tick_image = PhotoImage(file="Udemy/31-day/images/right.png")
-tick_button = Button(image=tick_image, highlightthickness=0, command=partial(change_word, "french"))
+tick_button = Button(image=tick_image, highlightthickness=0, command=change_word)
 tick_button.grid(row=1, column=1)
 
-# countdown(300)
+change_word()
 
 window.mainloop()
