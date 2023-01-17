1e238f6051d3b147b94d679b65aa89389c8621fb
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-17 12:30:49 -0300

    Introduction to datetime library

--- Udemy/32-day/Birthday Wisher (Day 32) start/main.py
+++ Udemy/32-day/Birthday Wisher (Day 32) start/main.py
@@ -1,15 +1,27 @@
 import smtplib
+import datetime as dt
 
-yahoo_email = "pruebaprogramacion99@yahoo.com"
-PASSWORD = "abcd1234()6789"
-gmail_email = "pruebaprogramacion5773@gmail.com"
-password_g = "abcd1234()"
-password_app_g = "jlcbzdoajvkrpsrq"
+# yahoo_email = "pruebaprogramacion99@yahoo.com"
+# PASSWORD = "abcd1234()6789"
+# gmail_email = "pruebaprogramacion5773@gmail.com"
+# password_g = "abcd1234()"
+# password_app_g = "jlcbzdoajvkrpsrq"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
+# with smtplib.SMTP("smtp.gmail.com") as connection:
 
-# Start tls will secure our conection and encrypt the email during its journey
-    connection.starttls()
-    connection.login(user=gmail_email, password=password_app_g)
-    connection.sendmail(from_addr=gmail_email, to_addrs=yahoo_email, msg="Subject:Hello\n\nThis is the body of my email")
-# connection.close()
+# # Start tls will secure our conection and encrypt the email during its journey
+#     connection.starttls()
+#     connection.login(user=gmail_email, password=password_app_g)
+#     connection.sendmail(from_addr=gmail_email, to_addrs=yahoo_email, msg="Subject:Hello\n\nThis is the body of my email")
+# # connection.close()
+
+now = dt.datetime.now()
+print(now)
+year = now.year
+print(year)
+month = now.month
+print(month)
+day_of_week = now.weekday()
+print(day_of_week) # Computers count from 0, so 0 is monday, 1 tuesday, etc..
+date_of_birth = dt.datetime(year= 2000, month= 12, day=10, hour=4)
+print(date_of_birth)
