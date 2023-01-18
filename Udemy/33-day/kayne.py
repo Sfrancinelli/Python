438f07ba5f0e3193a7b7add605191466cdfc94a7
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-18 11:48:06 -0300

    Kanye quotes aplication completed!

--- Udemy/33-day/kayne.py
+++ Udemy/33-day/kayne.py
@@ -1,9 +1,13 @@
 from tkinter import *
-
+import requests
 
 def get_quote():
-    pass
-    #Write your code here.
+    response = requests.get(url="https://api.kanye.rest")
+    response.raise_for_status()
+
+    data = response.json()
+    print(data)
+    canvas.itemconfig(quote_text, text=data["quote"])
 
 
 window = Tk()
