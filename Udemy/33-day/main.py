1bbfb516994c27673ec0c5e09f60e3b40a3a6ee9
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-18 12:44:53 -0300

    Working bith bigger APIs. Starting ISS Proyect!

--- Udemy/33-day/main.py
+++ Udemy/33-day/main.py
@@ -1,14 +1,41 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+# print(data["iss_position"])
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+
+# print(iss_position)
+
+parameters = {
+    "lat" : -34.641364,
+    "lng" : -58.406247,
+    "formatted" : 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-print(data["iss_position"])
+print(data)
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+time_now = datetime.now()
 
-iss_position = (longitude, latitude)
+print(time_now.hour)
+# print(sunrise, sunset)
 
-print(iss_position)
+# Get the hour alone from the response time. Splitting out the datetime.
+splitted_sunrise = sunrise.split("T")[1].split(":")[0]
+splitted_sunset = sunset.split("T")[1].split(":")[0]
+
+print(splitted_sunrise, splitted_sunset)
+
+
