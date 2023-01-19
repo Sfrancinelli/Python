94209224e4b2d2ce4eaa96905d39213da2b57747
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-19 11:19:49 -0300

    Starting with UI Interface for Quizz App!!

--- Udemy/34-day/main.py
+++ Udemy/34-day/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 count = 0
 count_q = 0
@@ -8,10 +9,10 @@ count_q = 0
 question_bank = []
 
 
-for _ in range(0, 10):
+for question in question_data:
 
-    question_text = question_data[count]['results'][count_q]['question']
-    question_answer = question_data[count]['results'][count_q]['correct_answer']
+    question_text = question['question']
+    question_answer = question['correct_answer']
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
     count_q += 1
@@ -19,8 +20,10 @@ for _ in range(0, 10):
 
 quiz = QuizBrain(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+ui = QuizInterface()
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- Udemy/34-day/ui.py
+++ Udemy/34-day/ui.py
@@ -1,2 +1,29 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score = Label(text="Score: 0", highlightthickness=0, bg=THEME_COLOR, fg="white")
+        self.score.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0)
+        self.q_text = self.canvas.create_text(150, 125, text="question", fill="black", font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2)
+
+        self.true_img = PhotoImage(file="Udemy/34-day/images/true.png")
+        self.false_img = PhotoImage(file="Udemy/34-day/images/false.png")
+
+        self.true_btn = Button(image=self.true_img, highlightthickness=0)
+        self.true_btn.grid(row=2, column=0)
+
+        self.false_btn = Button(image=self.false_img, highlightthickness=0)
+        self.false_btn.grid(row=2, column=1)
+
+        self.window.mainloop()
 
