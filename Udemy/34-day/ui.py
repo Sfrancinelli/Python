7590b072fa368643173226bc91f9842a07e39755
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-19 13:07:02 -0300

    UI Working and displaying question, gotta work on the buttons!

--- Udemy/34-day/ui.py
+++ Udemy/34-day/ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -13,7 +16,7 @@ class QuizInterface:
         self.score.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250, highlightthickness=0)
-        self.q_text = self.canvas.create_text(150, 125, text=f"question", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.q_text = self.canvas.create_text(150, 125, text="", fill=THEME_COLOR, font=("Arial", 16, "italic"), width=280)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=25)
 
         self.true_img = PhotoImage(file="Udemy/34-day/images/true.png")
@@ -25,5 +28,11 @@ class QuizInterface:
         self.false_btn = Button(image=self.false_img, highlightthickness=0)
         self.false_btn.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        question_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.q_text, text=question_text)
 
