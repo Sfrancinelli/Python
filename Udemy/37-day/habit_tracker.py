28c17f83842e94430d1ec0039b8ffd9e5ac4f051
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-26 12:27:13 -0300

    Learning bout PUT and DELETE http methods with pixela graph!

--- Udemy/37-day/habit_tracker.py
+++ Udemy/37-day/habit_tracker.py
@@ -30,12 +30,32 @@ graph_values = {
     "quantity" : "17.4"
 }
 
+# Create a Graph in pxela
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 
 # print(response.text)
 
-response = requests.post(url=graph_adding_end, json=graph_values, headers=headers)
+# Add information to the pixela graph
+# response = requests.post(url=graph_adding_end, json=graph_values, headers=headers)
 
+# print(response.text)
+
+modify_endpoint = f'{pixela_endpoint}/{USER}/graphs/{GRAPH_ID}/{yesterday.strftime("%Y%m%d")}'
+
+modify_json = {
+    "quantity" : "54.5"
+}
+
+# Modify information of a pixel in a pixela graph
+# response = requests.put(url=modify_endpoint, json=modify_json, headers=headers)
+
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USER}/graphs/{GRAPH_ID}/{yesterday.strftime('%Y%m%d')}"
+
+# Delete a pixel in a pixela graph
+response = requests.delete(url=delete_endpoint, headers=headers)
 print(response.text)
+
 
 
