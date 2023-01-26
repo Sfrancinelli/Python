cf684bdc9f498f58266aef10b71fb522de425ac7
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-01-26 17:50:33 -0300

    Working the way to add rows to the spreadsheet.

--- Udemy/38-day/workout_tracker.py
+++ Udemy/38-day/workout_tracker.py
@@ -1,7 +1,8 @@
 # x-app-id and x-app-key are required and need to be send as headers params.
 # x-remote-user-id shoud be set to 0 right now.
 import api_keys
-import requests 
+import requests
+import datetime
 
 GENDER ="MALE"
 WEIGHT_KG = "75.5"
@@ -28,9 +29,22 @@ params = {
     "age" : AGE,
 }
 
-response = requests.post(url=excercise_endpoint, json=params, headers=header)
+response = requests.post(url=excercise_endpoint, json=
+                         params, headers=header)
 response.raise_for_status()
 data = response.json()
 print(data)
 
 SPREADSHEET_ENDPOINT = api_keys.SHEET_ENDPOINT
+
+time_now = datetime.datetime.now()
+print(time_now.strftime("%d/%m/%Y")) # Ej: 26/01/2023
+
+try: 
+    print(data['excercises'][0])
+except:
+    print("could not print value 0")
+try:
+    print(data['exercises'][1])
+except:
+    print("could not print value 1")
