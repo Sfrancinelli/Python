6236f80ed3f4f7939e558b101816cd0b002be1eb
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-02-15 12:27:28 -0300

    Starting the code to get an IATA code from a city name!

--- Udemy/39-day/flight_search.py
+++ Udemy/39-day/flight_search.py
@@ -1,10 +1,17 @@
+import requests
+import api_keys
+from pprint import pprint
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     def __init__(self, city):
         self.city = city
         self.iataCode = None
+        self.API_KEY = api_keys.API_KEY
+        self.API_END = api_keys.API_END
 
     def get_iata_code(self, city):
         if city != '':
-            self.iataCode = "TESTING"
-            return self.iataCode
+            pass
+            # self.iataCode = "TESTING"
+            # return self.iataCode
