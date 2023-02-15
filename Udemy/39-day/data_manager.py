aabfe53426e5760801d406fb5855f744bd66319a
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-02-15 09:45:17 -0300

    Edited data_manager class to update values of the sheet

--- Udemy/39-day/data_manager.py
+++ Udemy/39-day/data_manager.py
@@ -7,13 +7,23 @@ class DataManager:
     def __init__(self):
         self.SHEET_ENDPOINT = api_keys.SHEET_ENDPOINT
         self.AUTH_STR = api_keys.AUTH
+        self.HEADERS = {'Authorization' : self.AUTH_STR}
 
     def get_data(self):
-        headers = {'Authorization' : self.AUTH_STR}
-        sheet_response = requests.get(url=self.SHEET_ENDPOINT, headers=headers)
+        sheet_response = requests.get(url=self.SHEET_ENDPOINT, headers=self.HEADERS)
         sheet_response.raise_for_status()
         sheet_data = sheet_response.json()
         return sheet_data['prices']
+    
+    def edit_data(self, row, column, value):
+        data = {
+            "prices" : {
+            column : value
+            }
+        }
+        response = requests.put(url=f"{self.SHEET_ENDPOINT}/{row}", headers=self.HEADERS, json=data)
+        response.raise_for_status()
+        print(f"Row {row} updated successfully!")
 
 if __name__ == "__main__":
     data_manager = DataManager()
