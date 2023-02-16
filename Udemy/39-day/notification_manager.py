9f93c6080ffd263b72929bdf797afa642fd9697c
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-02-16 14:50:49 -0300

    Gotta start with the Notification Manager class to send email or text

--- Udemy/39-day/notification_manager.py
+++ Udemy/39-day/notification_manager.py
@@ -1,3 +1,7 @@
+from flight_data import FlightData
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        """This class is responsible for sending notifications with the deal flight details."""
+        pass
