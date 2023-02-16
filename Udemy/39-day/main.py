65bfa28d8058d0003c0929dec57b9d00a499fb83
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-02-16 12:56:03 -0300

    Comments added

--- Udemy/39-day/main.py
+++ Udemy/39-day/main.py
@@ -8,22 +8,25 @@ sheet_data = data_manager.get_data()
 
 # print(sheet_data[0]['city'])
 
-# count = 0
+count = 0
 
-# for _ in sheet_data:
-#     if sheet_data[count]['iataCode'] == '':
-#         city = sheet_data[count]['city']
-#         flight_search = FlightSearch(city)
-#         iata_code = flight_search.get_iata_code(city)
-#         # print(city)
-#         # print(iata_code)
-#         sheet_data[count]['iataCode'] = iata_code
-#         # print(city)
-#         # print(sheet_data[count]['iataCode'])
-#     count += 1
+for _ in sheet_data:
+    if sheet_data[count]['iataCode'] == '':
+        city = sheet_data[count]['city']
+        flight_search = FlightSearch(city)
+        iata_code = flight_search.get_iata_code(city)
+        # print(city)
+        # print(iata_code)
+        sheet_data[count]['iataCode'] = iata_code
+        # print(city)
+        # print(sheet_data[count]['iataCode'])
+    count += 1
 
 # pprint(sheet_data[0]['city'])
 # pprint(sheet_data)
+
+# This line is commented so it doesn't trigger more API Calls (limit is 200 per month)
+# For the final code, the line below must be uncommented
 # data_manager.edit_data()
 
 flight_search = FlightSearch(sheet_data[0]['city'])
