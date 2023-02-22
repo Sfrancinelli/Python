a26164032b4d44f63a1c7817bcccdc966339c861
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-02-22 10:53:52 -0300

    Managed to send SMS if cheapest flight is found!

--- Udemy/39-day/main.py
+++ Udemy/39-day/main.py
@@ -1,12 +1,15 @@
 from data_manager import DataManager
 from pprint import pprint
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 data_manager = DataManager()
 sheet_data = data_manager.get_data()
 # pprint(sheet_data)
 
 # print(sheet_data[0]['city'])
+
+
 
 count = 0
 
@@ -33,7 +36,9 @@ flight_search = FlightSearch(sheet_data[0]['city'])
 
 for destination in sheet_data:
     flights = flight_search.search_flight(destination['iataCode'])
-
+    notification = NotificationManager()
+    if flights.price < destination['lowestPrice']:
+        notification.send_notification(price=flights.price, origin_city=flights.origin_city, origin_airport= flights.origin_airport, destination_city=flights.destination_city, destination_airport=flights.destination_airport, out_date=flights.out_date, return_date=flights.return_date)
     print("---------------------------------")
 
 
