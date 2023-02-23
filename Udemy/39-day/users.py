2eb26cf7948c878aacf36981573c8faaa44c9252
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-02-23 16:39:25 -0300

    Managed to save the new users into the users sheet

--- Udemy/39-day/users.py
+++ Udemy/39-day/users.py
@@ -5,13 +5,32 @@ from pprint import pprint
 USERS_ENDPOINT = api_keys.USERS_ENDPOINT
 
 AUTH = api_keys.AUTH
-headers = {'Authorization' : AUTH}
+HEADERS = {'Authorization' : AUTH}
 
-sheet_response = requests.get(url=USERS_ENDPOINT, headers=headers)
-sheet_response.raise_for_status()
-sheet_data = sheet_response.json()
-pprint(sheet_data['users'][0])
+# sheet_response = requests.get(url=USERS_ENDPOINT, headers=HEADERS)
+# sheet_response.raise_for_status()
+# sheet_data = sheet_response.json()
+# pprint(sheet_data['users'][0])
 
-# First Name Row = sheet_data['users'][0]['firstName']
-# Last Name Row = sheet_data['users'][0]['lastName']
-# Email Row = sheet_data['users'][0]['email']
+# First Name Column = sheet_data['users'][0]['firstName']
+# Last Name Column = sheet_data['users'][0]['lastName']
+# Email Column = sheet_data['users'][0]['email']
+
+print("Welcome to Sebastian's Flight Club.\nWe find the best flight deals and email you.")
+first_name = input("What's your first name?\n")
+last_name = input("What's your last name?\n")
+email = input("What's your email?\n")
+email_confirmation = input("Please type your email again.\n")
+
+if email == email_confirmation:
+    print("You're in the club!")
+    new_data = {
+        "user": {
+            "firstName" : first_name,
+            "lastName" : last_name,
+            "email" : email
+        }
+    }
+    response = requests.post(url=USERS_ENDPOINT, headers=HEADERS, json=new_data)
+    response.raise_for_status()
+    print(f"Sheet updated successfully!")
